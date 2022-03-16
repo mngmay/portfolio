ec0db0f5d57963696b611f337f71670bdb7eb2f0
May Ng <mayng@Mays-MacBook-Pro.local>
2022-03-16 12:47:46 -0700

    error message

--- components/Error404.tsx
+++ components/Error404.tsx
@@ -1,11 +1,22 @@
-import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import StyledExternalLink from './StyledExternalLink';
 
 const Error404 = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Typography variant="h1">Woops! Looks like that is missing</Typography>
+        <Typography variant="h1">Woops!</Typography>
+        <Typography>
+          {' '}
+          Looks like something may be missing. Use the navigation to try again.
+        </Typography>
+        <Typography>
+          If there was a link that is broken, please let me know by{' '}
+          <StyledExternalLink href="mailto: mngmay@gmail.com">
+            contacting me!
+          </StyledExternalLink>
+        </Typography>
       </Grid>
     </Grid>
   );
